e2e8f5c290624b9badc349fdb269092c6994fe5f
abbas <abbas22013@gmail.com>
2025-02-12 11:51:00 +0300

    finish

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const logger = require('morgan')
 const testJwtRouter = require('./controllers/test-jwt')
 const authRouter = require('./controllers/auth')
 const usersRouter = require('./controllers/users')
+// server.js
+
+const hootsRouter = require("./controllers/hoots.js");
 
 mongoose.connect(process.env.MONGODB_URI)
 
@@ -23,7 +26,7 @@ app.use(logger('dev'))
 app.use('/auth', authRouter)
 app.use('/test-jwt', testJwtRouter)
 app.use('/users', usersRouter)
-
+app.use("/hoots", hootsRouter);
 
 
 app.listen( process.env.PORT || 3000, () => {
